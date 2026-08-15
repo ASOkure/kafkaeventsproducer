da755615420d7128e8f1344d60aef35857f797dd
akanimo sam okure <akan.okure@gmail.com>
2026-08-15 22:03:08 +0100

    initial setup

---
+++ src/test/java/com/mantletechsolutions/kafkaeventsproducer/KafkaeventsproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantletechsolutions.kafkaeventsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaeventsproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
