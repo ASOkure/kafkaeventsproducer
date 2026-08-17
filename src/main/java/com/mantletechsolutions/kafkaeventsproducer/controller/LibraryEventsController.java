2377379dbb820e1ac2ad7eee959ed2fd37dd6059
akanimo sam okure <akan.okure@gmail.com>
2026-08-17 22:54:34 +0100

    add controller and implement libraryEvent end point

---
+++ src/main/java/com/mantletechsolutions/kafkaeventsproducer/controller/LibraryEventsController.java
@@ -0,0 +1,32 @@
+package com.mantletechsolutions.kafkaeventsproducer.controller;
+
+
+import com.mantletechsolutions.kafkaeventsproducer.domain.LibraryEvent;
+import com.mantletechsolutions.kafkaeventsproducer.domain.LibraryEventType;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LibraryEventsController {
+
+
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<?> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+     log.info("libraryEvent : {}", libraryEvent);
+        //invoke kafka producer
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+
+
+}
